edc342fc6c934d67025c6c65245060317de01758
TParro <thomas.parro@dcmail.ca>
2020-02-05 11:16:23 -0500

    Commenting enums and card class

--- Final Project Tester/Final Project Tester/Suit.cs
+++ Final Project Tester/Final Project Tester/Suit.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Final_Project_Tester
 {
+    /// <summary>
+    /// Enumerator of the suits in a deck of cards
+    /// </summary>
     public enum Suit
     {
         Spade,
