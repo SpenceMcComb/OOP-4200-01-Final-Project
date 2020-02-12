10afd21ddc242efb5e679517456b203104436bc8
Hamza <hamzak777@gmail.com>
2020-02-12 13:07:55 -0500

    player class commented out

--- Final Project Tester/Final Project Tester/Player.cs
+++ Final Project Tester/Final Project Tester/Player.cs
@@ -6,51 +6,51 @@ namespace Final_Project_Tester
 {
     public abstract class Player
     {
-        //instance
-        private String name = "";
-        private PlayerHand hand = new playerHand();
+        ////instance
+        //private String name = "";
+        //private PlayerHand hand = new playerHand();
 
-        private bool Attacking = false;
-        private bool Defending = false;
-
-
-        //class 
-        public const String DEFAULT_NAME = "Bruce Wayne";
-        public const PlayerHand DEFAULT_HAND = null;
-
-        //public const bool DEFAULT_ATTACK = false;
-        //public const bool DEFAULT_DEFEND = false;
+        //private bool Attacking = false;
+        //private bool Defending = false;
 
 
-        //default constructor
-        public Player()
-        {
-            setName(DEFAULT_NAME);
-            setHand(DEFAULT_HAND);
-            //setIsAttacking(DEFAULT_ATTACK);
-            //setIsDefending(DEFAULT_DEFEND);
-        }
+        ////class 
+        //public const String DEFAULT_NAME = "Bruce Wayne";
+        //public const PlayerHand DEFAULT_HAND = null;
 
-        //getters and setters
-        protected String getName()
-        {
-            return name;
-        }
+        ////public const bool DEFAULT_ATTACK = false;
+        ////public const bool DEFAULT_DEFEND = false;
 
-        protected void setName(String name)
-        {
-            this.name = name;
-        }
 
-        protected PlayerHand getHand()
-        {
-            return hand;
-        }
+        ////default constructor
+        //public Player()
+        //{
+        //    setName(DEFAULT_NAME);
+        //    setHand(DEFAULT_HAND);
+        //    //setIsAttacking(DEFAULT_ATTACK);
+        //    //setIsDefending(DEFAULT_DEFEND);
+        //}
 
-        protected void setHand(PlayerHand hand)
-        {
-            this.hand = hand;
-        }
+        ////getters and setters
+        //protected String getName()
+        //{
+        //    return name;
+        //}
+
+        //protected void setName(String name)
+        //{
+        //    this.name = name;
+        //}
+
+        //protected PlayerHand getHand()
+        //{
+        //    return hand;
+        //}
+
+        //protected void setHand(PlayerHand hand)
+        //{
+        //    this.hand = hand;
+        //}
     }
 }
 
