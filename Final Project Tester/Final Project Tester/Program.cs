eb7b387d56e30339cdd157cb3282a08a60ff5fd4
TParro <thomas.parro@dcmail.ca>
2020-02-19 14:46:28 -0500

    Adding to the program.cs to example the comparison operators

--- Final Project Tester/Final Project Tester/Program.cs
+++ Final Project Tester/Final Project Tester/Program.cs
@@ -7,14 +7,51 @@ namespace Final_Project_Tester
         static void Main(string[] args)
         {
             Deck theDeck = new Deck();
-            Card[] theCards = new Card[36];
 
             // Loop through the deck
             for (int i = 0; i < 36; i++)
             {
                 // Draw the cards and display them to the user
-                theCards[i] = theDeck.GetCard(i);
-                Console.WriteLine(theCards[i].ToString());
+                Console.WriteLine(theDeck.GetCard(i).ToString());
+            }
+
+            // Testing comparison operators
+            Console.WriteLine("\n=================================================================\nTesting Comparison Operators with Ace of Spades and Six of Spades\n=================================================================\n");
+
+            if (theDeck.GetCard(0) < theDeck.GetCard(8))
+            {
+                Console.WriteLine("{0} is less than {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT less than {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+
+            if (theDeck.GetCard(0) <= theDeck.GetCard(8))
+            {
+                Console.WriteLine("{0} is less than or equal to {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT less than or equal to {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+
+            if (theDeck.GetCard(0) > theDeck.GetCard(8))
+            {
+                Console.WriteLine("{0} is greater than {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT greater than {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+
+            if (theDeck.GetCard(0) >= theDeck.GetCard(8))
+            {
+                Console.WriteLine("{0} is greater than or equal to {1}", theDeck.GetCard(0), theDeck.GetCard(8));
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT greater than or equal to {1}", theDeck.GetCard(0), theDeck.GetCard(8));
             }
 
             Console.ReadKey();
